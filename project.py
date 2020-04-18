bd58bbd2cc77e0f55f4663cfb48adaddc361ed36
MalMyeong <61974115+MalMyeong@users.noreply.github.com>
2020-04-18 21:27:19 +0900

    Update project.py
    gridFS 사용 후 db에 fs.chunk, fs.file 생성 binary 데이터 저장되어있는데
    upload db안에 있지 않고 개별적으로 db가 생성되어있는 issue, server error

--- project.py
+++ project.py
@@ -1,13 +1,15 @@
 
 from flask import Flask, render_template, jsonify, request
-
-
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
 from pymongo import MongoClient
+from gridfs import GridFS
+from bson import objectid
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
+fs = GridFS(db)
 collection = db.upload
 
 
@@ -16,8 +18,9 @@ def home():
     return render_template('project_page1.html')
 
 @app.route('/upload', methods=['POST'])
-def upload():
-    photo = request.form['photo']
+def upload_data():
+    file = request.files['file']
+    photo = fs.put(file, filename=file.filename, encoding='utf-8')
     title = request.form['title']
     main_value = request.form['main_value']
     equip = request.form['equip']
@@ -32,9 +35,12 @@ def upload():
         'data': data
     }
     db.upload.insert_one(doc)
-    return jsonify({'result':'success'})
+    return render_template('project_page1.html')
 
-
+@app.route('/upload', methods=['GET'])
+def read_data():
+    read = list(db.upload.find({},{'_id':0}))
+    return jsonify({'result':'success', 'read':read})
 
 if __name__ == '__main__':
     app.run('localhost', port=5000, debug=True)
