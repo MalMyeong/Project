0d2cc438f7fe8876d1b832d6a7906a4ba8a58546
MalMyeong <61974115+MalMyeong@users.noreply.github.com>
2020-04-21 16:53:03 +0900

    Update uploads.py
    입력한 옵션과 일치하는 데이터 값 불러오기 구현
    삭제하기 눌렀을 때 삭제하려는 데이터와 이름이 같은 데이터 모두 삭제되는 문제 해결

--- uploads.py
+++ uploads.py
@@ -21,10 +21,10 @@ def search_data():
     main_value = request.form['main_value']
     equipment = request.form['equipment']
     sub_value = request.form['sub_value']
-    find_main = db.uploads.find({'main_value':main_value})
-    find_equip = find_main({'equipment':equipment})
-    list(find_equip({'sub_value':sub_value}))
-    return jsonify({'result':'success'})
+    result = db.uploads.find({'$and':[{'main_value':main_value,'equipment':equipment,'sub_value':sub_value}]})
+    for i in result:
+        print(i['data'])
+    return render_template('project_page2.html')
 
 
 
@@ -58,7 +58,7 @@ def delete_data():
     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
     title = request.form['title']
     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-    db.uploads.remove({'title':title})
+    db.uploads.delete_one({'title':title})
     # 3. 성공하면 success 메시지를 반환합니다.
     return jsonify({'result': 'success'})
 
